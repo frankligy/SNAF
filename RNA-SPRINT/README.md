6532554b6c0c4a945739f38f950f70c64a409ab7
Guangyuan(Frank) Li <59619831+frankligy@users.noreply.github.com>
2024-01-12 11:40:02 -0500

    polish RNA-SPRINT

--- RNA-SPRINT/README.md
+++ RNA-SPRINT/README.md
@@ -2,9 +2,54 @@
 
 RNA-SPRINT is an approach to estimate the RNA binding protein (RBP) activity in each sample. The idea of RNA-SPRINT is simple, we first built a prior network representing the confident relationship between RBP and its targerted splicing junction in K562 data from large-scale knock-down evidence and eCLIP-Seq binding evidence. Using this prior network as basis, we ran Multivariate Decision Tree (MDT) from `decoupler-py` package, which shows consistent better performance in the independent benchmark in HepG2 cell line inference.
 
+![workflow](./RNA-SPRINT.png)
+
 The input of RNA-SPRINT is the EventAnnotation file from AltAnalyze results (n_splicing_junctions * n_samples), the output is a `mdt_estimate` file (n_RBPs * n_samples) containing the activity for each RBP in each sample. For the EventAnnotation file, you can follow the first step of the SNAF tutorial, EventAnnotation file will be located at
 `altanalyze_output/AltResults/AlternativeOutput/Hs_RNASeq_top_alt_junctions-PSI_EventAnnotation.txt`.
 
-We provided a `yml` file to reproduce the environment, but you can build youself based on the `RNA-SPRINT.py` code, should be pretty straightforward.
+## Dependency
 
-To run that, you just need to change the capital variables indicated in the `RNA-SPRINT.py` file. And also download the prior network file (`prior.tsv`) from [Synapse](https://www.synapse.org/#!Synapse:syn53038679).
+```
+# using conda to build a env and pip install following
+python=3.8
+decoupler==1.2.0
+seaborn==0.12.0
+skranger
+```
+
+We provided a `yml` file to fully reproduce the development environment, I developed this on linux.
+
+## Run the program
+
+To run that, first you just need to change the capital variables indicated in the `RNA-SPRINT.py` file. And also download the prior network file (`prior.tsv`) from [Synapse](https://www.synapse.org/#!Synapse:syn53038679).
+
+Now you can run like following:
+
+```bash
+python RNA_SPRINT.py --splicing ./osteosarcoma_Hs_RNASeq_top_alt_junctions-PSI_EventAnnotation.txt 
+--prior ./prior.tsv 
+--outdir . 
+--meta ./meta.txt 
+```
+
+`meta` is a text file with all the metadata information associated with each sample, to facilitate direct visualization on [Morpheus](https://software.broadinstitute.org/morpheus/).
+
+Full help prompt is as below:
+
+```bash
+usage: RNA_SPRINT.py [-h] [--splicing SPLICING] [--prop PROP] [--cv CV] [--prior PRIOR] [--meta META] [--outdir OUTDIR]
+
+RNA-SPRINT command line
+
+optional arguments:
+  -h, --help           show this help message and exit
+  --splicing SPLICING  path to the splicing EventAnnotation file
+  --prop PROP          Only include splicing that present above that proportion cutoff
+  --cv CV              Only include splicing that preserve covariance coeffieint above this cv cutoff
+  --prior PRIOR        path to the downloaded k562 prior network file
+  --meta META          path to a metadata file to facilitate the visualization, two or more column, first is sample name, other columns are different
+                       metadata, with informati
+  --outdir OUTDIR      path to output directory
+```
+
+Contact me for any question, I'll be responsive!
--- RNA-SPRINT/RNA_SPRINT.py
+++ RNA-SPRINT/RNA_SPRINT.py
@@ -1,4 +1,4 @@
-#!/data/salomonis-archive/FASTQs/NCI-R01/ENCODE_KD_fastq_frank/decoupler_env/bin/python3.8
+#!/gpfs/data/yarmarkovichlab/Frank/SNAF_ecosystem/RNA-SPRINT/rna_sprint_env/bin/python3.8
 
 import decoupler as dc
 import matplotlib.pyplot as plt
@@ -7,20 +7,13 @@ import pandas as pd
 import numpy as np
 import os,sys
 from tqdm import tqdm
-from adjustText import adjust_text
 import matplotlib as mpl
+import argparse
 
 mpl.rcParams['pdf.fonttype'] = 42
 mpl.rcParams['ps.fonttype'] = 42
 mpl.rcParams['font.family'] = 'Arial'
 
-
-# you only need to change this captical variable, a file called mdt_estimate.tsv will show estimated RBP activity for each sample 
-EVENT_ANNOTATION_FILE_PATH = '../TCGA_melanoma/Hs_RNASeq_top_alt_junctions-PSI_EventAnnotation.txt'
-PROPORTION_CUTOFF = 0.75
-VARIATION_CUTOFF = 0.1
-K562_PRIOR_NETWORK_PATH = '/data/salomonis-archive/FASTQs/NCI-R01/ENCODE_KD_fastq_frank/altanalyze_output/HepG2_inference/prior.tsv'
-OUTPUT_DIR = '.'
 
 # preprocessing to get necessary input files, code derived from https://github.com/frankligy/splice-inferelator/blob/main/scripts/prepare_inference.py
 def disentangle_uid(uid,add_stable_id=False,add_symbol=False):
@@ -40,30 +33,74 @@ def median_impute(x):
     result = np.nan_to_num(x.values,nan=med)
     return result
 
-ee = pd.read_csv(EVENT_ANNOTATION_FILE_PATH,sep='\t',index_col='UID').iloc[:,10:]
-ee.index = [disentangle_uid(item,add_symbol=True) for item in ee.index]
-prop = 1- np.count_nonzero(ee.isna().values,axis=1)/ee.shape[1]  # 101974 rows x 472 columns
-ee = ee.loc[prop>PROPORTION_CUTOFF,:]   # 59134 rows x 472 columns
-original_columns = ee.columns
-ee = ee.apply(func=median_impute,axis=1,result_type='expand')
-ee.columns = original_columns
-cv = ee.apply(func=lambda x:x.values.std()/x.values.mean(),axis=1,result_type='reduce').values
-ee = ee.loc[cv>VARIATION_CUTOFF,:]
-ee.columns = original_columns   # 34683 rows x 472 columns
-ee.T.to_csv('expr.tsv',sep='\t')
+def main(args):
+    EVENT_ANNOTATION_FILE_PATH = args.splicing
+    PROPORTION_CUTOFF = args.prop
+    VARIATION_CUTOFF = args.cv
+    K562_PRIOR_NETWORK_PATH = args.prior
+    OUTPUT_DIR = args.outdir
+    meta = args.meta
+
+    if not os.path.exists(OUTPUT_DIR):
+        os.mkdir(OUTPUT_DIR)
+    root_path = OUTPUT_DIR
+
+    ee = pd.read_csv(EVENT_ANNOTATION_FILE_PATH,sep='\t',index_col='UID').iloc[:,10:]
+    print('loaded splicing matrix: {}'.format(ee.shape))
+    ee.index = [disentangle_uid(item,add_symbol=True) for item in ee.index]
+    prop = 1- np.count_nonzero(ee.isna().values,axis=1)/ee.shape[1]  # 101974 rows x 472 columns
+    ee = ee.loc[prop>PROPORTION_CUTOFF,:]   # 59134 rows x 472 columns
+    print('After proportion cutoff: {}'.format(ee.shape))
+    original_columns = ee.columns
+    ee = ee.apply(func=median_impute,axis=1,result_type='expand')
+    ee.columns = original_columns
+    cv = ee.apply(func=lambda x:x.values.std()/x.values.mean(),axis=1,result_type='reduce').values
+    ee = ee.loc[cv>VARIATION_CUTOFF,:]
+    print('After variation cutoff: {}'.format(ee.shape))
+    ee.columns = original_columns   # 34683 rows x 472 columns
+    ee.T.to_csv(os.path.join(OUTPUT_DIR,'expr.tsv'),sep='\t')
 
 
-expr_path = 'expr.tsv'
-prior_path = K562_PRIOR_NETWORK_PATH
-root_path = OUTPUT_DIR
-expr = pd.read_csv(expr_path,sep='\t',index_col=0)  # 472 rows x 34683 columns
-prior = pd.read_csv(prior_path,sep='\t',index_col=0)  # 73157 rows x 221 columns
-common_events = list(set(expr.columns).intersection(set(prior.index)))
-expr = expr.loc[:,common_events]   # 472 rows x 17836 columns
-prior = prior.loc[common_events,:]  # 17836 rows x 221 columns
-net = prior.stack().reset_index()
-net.columns = ['target','source','weight']
-net = net.loc[net['weight']!=0,:]
+    expr_path = os.path.join(OUTPUT_DIR,'expr.tsv')
+    prior_path = K562_PRIOR_NETWORK_PATH
+    expr = pd.read_csv(expr_path,sep='\t',index_col=0)  # 472 rows x 34683 columns
+    prior = pd.read_csv(prior_path,sep='\t',index_col=0)  # 73157 rows x 221 columns
+    common_events = list(set(expr.columns).intersection(set(prior.index)))
+    print('number of valid splicing event in inference: {}'.format(len(common_events)))
+    print('inference started')
+    expr = expr.loc[:,common_events]   # 472 rows x 17836 columns
+    prior = prior.loc[common_events,:]  # 17836 rows x 221 columns
+    net = prior.stack().reset_index()
+    net.columns = ['target','source','weight']
+    net = net.loc[net['weight']!=0,:]
 
-estimate = dc.run_mdt(mat=expr,net=net)
-estimate.to_csv(os.path.join(root_path,'mdt_estimate.txt'),sep='\t')
+    estimate = dc.run_mdt(mat=expr,net=net)
+    estimate.to_csv(os.path.join(root_path,'mdt_estimate.txt'),sep='\t')
+    print('inference ended, adding metadata')
+
+    # add metadata
+    group = pd.read_csv(meta,index_col=0,sep='\t')
+    all_meta = {}
+    for c in group.columns:
+        all_meta[c] = group[c].to_dict()
+    estimate = pd.read_csv(os.path.join(root_path,'mdt_estimate.txt'),sep='\t',index_col=0).T
+    mi_sample = tuple(estimate.columns.tolist())
+    mi_meta = {}
+    for k,v in all_meta.items():
+        mi_meta[k] = tuple([v[item] for item in mi_sample])
+    mi_array = [mi_sample,*mi_meta.values()]
+    mi_array_name = ['sample'] + [*mi_meta.keys()]
+    mi = pd.MultiIndex.from_arrays(arrays=mi_array,names=mi_array_name)
+    estimate.columns = mi
+    estimate.to_csv(os.path.join(root_path,'mdt_estimate_morpheus.txt'),sep='\t')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='RNA-SPRINT command line')
+    parser.add_argument('--splicing',type=str,default=None,help='path to the splicing EventAnnotation file')
+    parser.add_argument('--prop',type=float,default=0.75,help='Only include splicing that present above that proportion cutoff')
+    parser.add_argument('--cv',type=float,default=0.1,help='Only include splicing that preserve covariance coeffieint above this cv cutoff')
+    parser.add_argument('--prior',type=str,default=None,help='path to the downloaded k562 prior network file')
+    parser.add_argument('--meta',type=str,default=None,help='path to a metadata file to facilitate the visualization, two or more column, first is sample name, other columns are different metadata, with informati')
+    parser.add_argument('--outdir',type=str,default=None,help='path to output directory')
+    args = parser.parse_args()
+    main(args)
